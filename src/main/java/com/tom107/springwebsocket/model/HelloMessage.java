70d55e6ec754049bb3c155f6847b66566775debc
Tom1072 <lamvutungvt@gmail.com>
2022-02-12 20:15:41 -0500

    Hooked backend with frontend

---
+++ src/main/java/com/tom107/springwebsocket/model/HelloMessage.java
@@ -0,0 +1,12 @@
+package com.tom107.springwebsocket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+    private String name;
+}
